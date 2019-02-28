6529fe9b45ed2efa5aa55d1a561e427b9f16dc50
craig-gaskill <cagstsolutions@gmail.com>
2019-02-27 20:50:46 -0600

    Added styling for primary pages so they fill the remaining space available.

--- src/app/feature/maintenance/maintenance.component.ts
+++ src/app/feature/maintenance/maintenance.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'swkroa-maintenance',
   templateUrl: './maintenance.component.html',
   styleUrls: ['./maintenance.component.scss']
 })
--- src/app/feature/memberships/memberships.component.ts
+++ src/app/feature/memberships/memberships.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  templateUrl: './memberships.component.html'
+  templateUrl: './memberships.component.html',
+  styleUrls: ['./memberships.component.scss']
 })
 export class MembershipsComponent {
 }
